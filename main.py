5fcfb4ad8d92cb5ad2818540f657a8ed69015384
Jeffrey <jefbfitzp33@outlook.com>
2024-04-27 11:33:43 -0600

    add chapters to phase 0

---
+++ main.py
@@ -0,0 +1,33 @@
+##### this page is not to have function python code.  It is merely used as a syntax reference.
+
+<break code>
+
+
+#testing collapsable arrows
+first layer a
+    second layer aa
+    second layer ab
+first layer b
+    second layer ba 
+    second layer bb
+
+
+phase0
+    setup
+    cli
+    expressions
+    statements
+    bundling
+    data structures
+    HTML
+    CSS
+    DOM
+    Events
+    Git
+    Project
+phase1
+phase2
+phase3
+phase4
+phase5
+algo
