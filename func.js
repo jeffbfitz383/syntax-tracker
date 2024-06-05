8caec99369b75af7edeabe7c840544796d02c6c9
Jeffrey <jefbfitzp33@outlook.com>
2024-06-04 23:32:58 -0600

    change function added

--- func.js
+++ func.js
@@ -2,4 +2,15 @@ function say(input){
     console.log(input)
 }
 
+const numb1 = 8
+arrayOne =[1,1,2,3,5,8,13,21,34,55]
+
 say("Hello World")
+say(numb1)
+say(arrayOne)
+
+function change(amount){
+    say(`Your change is ${amount}`)
+}
+
+change(29.28)
